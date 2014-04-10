0b6f1d94ad22d9f320ea7d694f0ffa279a144c92
thehuntclub <thehuntclub@neo.tamu.edu>
2014-04-10 16:06:29 -0500

    Nick's changes

--- Server Prototype/Timer.js
+++ Server Prototype/Timer.js
@@ -4,24 +4,30 @@ function Timer(){
 	
 	this.min = 0;
 	this.sec = 0;
+	this.tenth = 0;
 }
 
 Timer.prototype.update = function(){
-	setInterval(function(){Update()},1000);
+	setInterval(function(){Update()},100);
 	//console.log(timer.tenth);
 }
 
 Timer.prototype.clearTime = function(){
 	timer.min=0;
 	timer.sec=0;
+	timer.tenth=0;
 }
 
 function Update(){
 	if(!myGame.gameManager.pause){
-		timer.sec++;
+		timer.tenth++;
+		if(timer.tenth==10){
+			timer.tenth=0;
+			timer.sec++;
+		}	
 		if(timer.sec==60){
 			timer.sec=0;
-			timer.min+=1;
+			timer.min++;
 		}
 	}
 }
