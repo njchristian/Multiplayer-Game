4561fb071fe488794203b276eee1928498a19d7b
chrisdlangham <chris.langham32@gmail.com>
2014-04-28 22:14:12 -0500

    comment added to game.js

--- Server Prototype/Game.js
+++ Server Prototype/Game.js
@@ -11,7 +11,7 @@
 	Requires Node.js and socket.io
 */
 
-
+//Game constructor. Its takes a graphics object, a socket object, and the username
 function Game( g, s, n ){
 
 	this.graphics = g;
@@ -20,14 +20,17 @@ function Game( g, s, n ){
 	
 	this.name = n;
 	
+	//game starts at the main menu
 	this.isOnMenu = true;
 	
+	//create a gameManager object and a menuManager object
 	this.gameManager = new GameManager( this, g, this.socket, this.name );
 	this.menuManager = new MenuManager( this, g, this.socket, this.name );
 	timer.update();
 	
 }
 
+//start the game object. call the run function until the game is over
 Game.prototype.start = function(){
 
 	this.isOnMenu = true;
@@ -36,12 +39,14 @@ Game.prototype.start = function(){
 	
 }
 
+//end the game object
 Game.prototype.end = function(){
 
 	clearInterval( this.run );
 
 }
 
+//return the game object to the main menu
 Game.prototype.returnToMenu = function(){
 
 	this.isOnMenu = true;
@@ -50,8 +55,7 @@ Game.prototype.returnToMenu = function(){
 
 }
 
-
-
+//function for choose what game type is chosen. It take the game mode and the difficulty which is a integer value
 function goToGame( gm, difficulty ){
 	
 	var socket = myGame.socket;
@@ -70,18 +74,18 @@ function goToGame( gm, difficulty ){
 				//myGame.gameManager.drawWait( myGame.gameManager.g );
 			}
 		});
-		
+	
+	//wait until a opponent is ready to play
 		socket.on(
 		'opponentForRace',
-		function(message) {
-			//if (message) {
+		function(message) {	
 				myGame.menuManager.isWaiting = false;
 				myGame.isOnMenu = false;
 				myGame.gameManager.newGame( gm );
-			//}
 		});
 	}
-	// challenge --------------------------needs finishing TODO
+	
+	//handle multiplayer challenge wait message
 	else if (gm == MULTI_CHALLENGE) {
 		socket.emit('mp_ch', { user_name: myGame.name });
 		
@@ -96,40 +100,48 @@ function goToGame( gm, difficulty ){
 				}
 		});
 		
+		//wait until a opponent is ready to play
 		socket.on(
 			'opponentForChallenge',
 			function(message) {
-				//if (message) {
 					myGame.menuManager.isWaiting = false;
 					myGame.isOnMenu = false;
 					myGame.gameManager.newGame( gm );
-				//}
 		});
 	}
 	// single player
 	else if (gm == TIME_TRIAL) {
+		//tell server who is playing the game
 		socket.emit('sp_tt', { user_name: myGame.name });
 		myGame.isOnMenu = false;
 		myGame.menuManager.selectingTTD = false;
 		console.log("Game: " + difficulty);
+		//create game
 		myGame.gameManager.newGame( gm, difficulty );
 	}
+	//single player challenge mode
 	else if (gm == SINGLE_CHALLENGE) {
+	//tell server who is playing the game
 		socket.emit('sp_ch', { user_name: myGame.name });
 		myGame.isOnMenu = false;
+		//create game
 		myGame.gameManager.newGame( gm );
 	
+	//tutorial mode
 	}else if( gm == TUTORIAL ){
 	
 		myGame.isOnMenu = false;
+		//create game
 		myGame.gameManager.newGame( gm );
 	
 	}
 	
+	//flag saying the player in not on the single or multiplayer mode
 	myGame.menuManager.onSPMenu = false;
 	myGame.menuManager.onMPMenu = false;
 }
 
+//update function. call update on the gameManager and menuManager
 Game.prototype.update = function(){
 
 	if( !this.isOnMenu ){
@@ -139,6 +151,7 @@ Game.prototype.update = function(){
 	}
 }
 
+//run is the function that will update everything and continue gameplay
 var run = function( ){
 
 	myGame.update();
@@ -146,6 +159,7 @@ var run = function( ){
 
 }
 
+//draw the graphics of the menuManager and gameManager
 Game.prototype.draw = function(){
 
 	if( this.isOnMenu ){
